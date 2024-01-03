5acceb85f6ae7e7c50dda17b92a9c8dd472e2279
vimal772 <vimal115077286@gmail.com>
2024-01-03 12:43:47 +0530

    code reduced

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const img = document.createElement('img')
-// fetch('http://api.weatherapi.com/v1/current.json?key=c7f099f4e3bb448791561817232212&q=London&aqi=no',{mode: 'cors'}).then((respone)=>{
-//     return respone.json()
-// }).then((result)=>{
-//     console.log(result.location)
-// })
-
-
 const weatherBtn = document.querySelector('.btn-weather')
+const checkBox = document.querySelector('#converter')
+
+checkBox.addEventListener('change',()=> {
+    const isChecked = checkBox.checked
+    converter(checkBox,isChecked)
+})
 
 weatherBtn.addEventListener('click',()=>{
     let inputValue = document.querySelector('input[type="text"]')
@@ -20,21 +19,59 @@ async function loadweather(location) {
     const weatherKey = 'c7f099f4e3bb448791561817232212';
     const path = await fetch(`http://api.weatherapi.com/v1/current.json?key=${weatherKey}&q=${location}&aqi=no`,{mode: 'cors'})
     const respone = await path.json()
-    console.log(respone)
-    displayLocation(respone);
-    displayWeather(respone)
     const result = respone.current.condition.text
+    displayLocation(respone);
+    fetchWeather(respone)
     getGif(result)
 }
 
-function displayWeather(respone) {
+async function getGif(name) {
+    if(name === 'Partly cloudy') name = 'cloudy'
+    if(name === 'Clear') name = 'clear sky'
+    if(name === 'Light drizzle') name = 'drizzle weather'
+    const gif = document.querySelector('.gif-wrap')
+    const result = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=eV8bEro9RWqY8fa5txBesSTQGfaNyFBB&s=${name}`,{mode: 'cors'})
+    const respone = await result.json()
+    img.classList.add('gif-img')
+    img.src = respone.data.images.original.url
+    gif.appendChild(img) 
+}
+
+function fetchWeather(respone) {
     const currentWeather = {
         cloud: respone.current.cloud,
         temp: respone.current.temp_c,
         humidity: respone.current.humidity,
         wind: respone.current.wind_kph
     }
-    console.log(currentWeather);
+    displayWeather(currentWeather)
+}
+
+function displayLocation(respone) {
+    const name  = respone.location.name
+    const country = respone.location.country
+    const wrap = document.querySelector('.location-wrap')
+    while(wrap.firstChild){
+        wrap.removeChild(wrap.firstChild)
+    }
+
+    const h1 = document.createElement('h1')
+    const h3 = document.createElement('h3')
+    h1.textContent = name
+    h3.textContent = country
+    wrap.appendChild(h1)
+    wrap.appendChild(h3)
+    
+}
+
+
+function converter(checkBox,isChecked) {
+    if(isChecked){
+
+    }
+}
+
+function displayWeather(currentWeather){
     const wrap = document.querySelector('.info-wrap')
     while(wrap.firstChild){
         wrap.removeChild(wrap.firstChild)
@@ -54,35 +91,4 @@ function displayWeather(respone) {
     wrap.appendChild(temp)
     wrap.appendChild(humidity)
     wrap.appendChild(wind)
-}
-
-function displayLocation(respone) {
-    const name  = respone.location.name
-    const country = respone.location.country
-    console.log(name, country);
-    const wrap = document.querySelector('.location-wrap')
-
-    while(wrap.firstChild){
-        wrap.removeChild(wrap.firstChild)
-    }
-
-    const h1 = document.createElement('h1')
-    h1.textContent = name
-    const h3 = document.createElement('h3')
-    h3.textContent = country
-    wrap.appendChild(h1)
-    wrap.appendChild(h3)
-    
-}
-
-async function getGif(name) {
-    if(name === 'Partly cloudy') name = 'cloudy'
-    if(name === 'Clear') name = 'weather clear'
-    if(name === 'Light drizzle') name = 'drizzle weather'
-    const result = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=eV8bEro9RWqY8fa5txBesSTQGfaNyFBB&s=${name}`,{mode: 'cors'})
-    const respone = await result.json()
-    img.classList.add('gif-img')
-    img.src = respone.data.images.original.url
-    const gif = document.querySelector('.gif-wrap')
-    gif.appendChild(img) 
 }
